a6e41384064d755e22664bf3053e1f47d344f6f2
Dianliang233 <dianliang233@gmail.com>
2023-12-12 16:42:40 +0800

    Add workflow

--- src/tools/exampleCounter/App.vue
+++ src/tools/exampleCounter/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { CdxButton } from '@wikimedia/codex'
+import Field from '@/components/Field.vue'
 const count = ref(0)
 const increment = () => count.value++
 </script>
