9b8ec7a051f298f232fbacb904cd93a9964d275c
Dianliang233 <dianliang233@gmail.com>
2023-12-13 13:55:53 +0800

    Deploy to prod

--- src/tools/blockDistribution/main.ts
+++ src/tools/blockDistribution/main.ts
@@ -4,19 +4,13 @@
 import * as vue from 'vue'
 import App from './App.vue'
 
-const targetEl = document.querySelector(
-  '.mcw-calc[data-type="blockDistribution"]'
-)
+const targetEl = document.querySelector('.mcw-calc[data-type="blockDistribution"]')
 const createApp =
   process.env.NODE_ENV === 'development'
     ? vue.createApp
     : // @ts-expect-error
       vue.createMwApp || vue.createApp
 createApp(App, {
-  blocks: targetEl?.getAttribute('data-blocks')?.split(',') ?? [
-    'minecraft:diamond_ore',
-  ],
-  blockNames: targetEl?.getAttribute('data-block-names')?.split(',') ?? [
-    'Diamond Ore',
-  ],
+  blocks: targetEl?.getAttribute('data-blocks')?.split(',') ?? ['minecraft:diamond_ore'],
+  blockNames: targetEl?.getAttribute('data-block-names')?.split(',') ?? ['Diamond Ore']
 }).mount(targetEl)
