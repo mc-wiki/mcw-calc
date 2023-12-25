57f6e8ce0f9d47b6deccc98d8d794251bdb30545
Dianliang233 <dianliang233@gmail.com>
2023-12-25 16:05:16 +0800

    Make type check pass

--- src/tools/netherCoords/main.ts
+++ src/tools/netherCoords/main.ts
@@ -5,10 +5,7 @@
 import * as vue from 'vue'
 import App from './App.vue'
 
-const targetEl = document.querySelector('.mcw-calc[data-type="netherCoords"]')
+const targetEl = document.querySelector('.mcw-calc[data-type="netherCoords"]')!
 const createApp =
-  process.env.NODE_ENV === 'development'
-    ? vue.createApp
-    : // @ts-expect-error
-      vue.createMwApp || vue.createApp
+  process.env.NODE_ENV === 'development' ? vue.createApp : vue.createMwApp || vue.createApp
 createApp(App).mount(targetEl)
