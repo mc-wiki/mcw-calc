457942bf3c47c7b3562b158afa040421afa66d38
Leo-768 <66005818+Leo-768@users.noreply.github.com>
2024-02-25 02:46:10 +0800

    typo

--- src/tools/blockDistribution/main.ts
+++ src/tools/blockDistribution/main.ts
@@ -9,6 +9,6 @@ const targetEl = document.querySelector('.mcw-calc[data-type="blockDistribution"
 const createApp =
   process.env.NODE_ENV === 'development' ? vue.createApp : vue.createMwApp || vue.createApp
 createApp(App, {
-  blocks: targetEl?.getElementsByClassName('mcw-clac-parameter-blocks')[0]?.textContent?.split(',') ?? ['minecraft:diamond_ore'],
-  blockNames: targetEl?.getElementsByClassName('mcw-clac-parameter-block-names')[0]?.textContent?.split(',') ?? ['Diamond Ore'],
+  blocks: targetEl?.getElementsByClassName('mcw-calc-parameter-blocks')[0]?.textContent?.split(',') ?? ['minecraft:diamond_ore'],
+  blockNames: targetEl?.getElementsByClassName('mcw-calc-parameter-block-names')[0]?.textContent?.split(',') ?? ['Diamond Ore'],
 }).mount(targetEl)
