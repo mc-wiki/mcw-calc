6ebd0bab0e3fb0de2b48d15e09d4668deff636bc
Leo-768 <leo768leo768@gmail.com>
2024-02-25 02:01:17 +0800

    Change the way parameters are passed

--- src/tools/blockDistribution/main.ts
+++ src/tools/blockDistribution/main.ts
@@ -9,6 +9,6 @@ const targetEl = document.querySelector('.mcw-calc[data-type="blockDistribution"
 const createApp =
   process.env.NODE_ENV === 'development' ? vue.createApp : vue.createMwApp || vue.createApp
 createApp(App, {
-  blocks: targetEl?.getAttribute('data-blocks')?.split(',') ?? ['minecraft:diamond_ore'],
-  blockNames: targetEl?.getAttribute('data-block-names')?.split(',') ?? ['Diamond Ore'],
+  blocks: targetEl?.getElementsByClassName('mcw-clac-parameter-blocks')[0]?.textContent?.split(',') ?? ['minecraft:diamond_ore'],
+  blockNames: targetEl?.getElementsByClassName('mcw-clac-parameter-block-names')[0]?.textContent?.split(',') ?? ['Diamond Ore'],
 }).mount(targetEl)
