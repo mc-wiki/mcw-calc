c804dc626a66faf0e0566889d8f4208703cec04c
YanisBft <yanis.briffaut@gmail.com>
2024-02-27 10:26:37 +0100

    Move strings to locales in used tools

---
+++ src/tools/armorColor/locales.ts
@@ -0,0 +1,13 @@
+export default {
+    en: {
+        'armorColor.title': 'Calculate dye sequence for a leather (horse) armor color',
+        'armorColor.java': 'Java Edition',
+        'armorColor.bedrock': 'Bedrock Edition',
+        'armorColor.color': 'Color:',
+        'armorColor.calculate': 'Calculate',
+        'armorColor.sequence': 'Sequence:',
+        'armorColor.sequence.help': 'You should dye the armor using all dyes at once.',
+        'armorColor.dE': 'dE',
+        'armorColor.dE.help': 'Delta E is a measure of color proximity. Lower is better. Values ≤1.0 means the difference is not perceptible by human eyes.',
+    },
+}
