b336564c5e96c72757a045b305eac76324fa84d3
YanisBft <yanis.briffaut@gmail.com>
2024-03-24 18:36:32 +0100

    Add maceDamage tool

---
+++ src/tools/maceDamage/locales.ts
@@ -0,0 +1,8 @@
+export default {
+  en: {
+    'maceDamage.title': 'Calculate mace damage',
+    'maceDamage.fallHeight': 'Fall height in blocks:',
+    'maceDamage.critical': 'Critical attack',
+    'maceDamage.damage': 'Damage done:',
+  },
+}
