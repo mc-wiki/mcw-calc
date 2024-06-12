4c5488c0dd43112b239d44572b83f703a6fbd68e
Dianliang233 <dianliang233@gmail.com>
2024-06-12 18:00:47 +0800

    Fix mace calculator

--- src/tools/maceDamage/App.vue
+++ src/tools/maceDamage/App.vue
@@ -25,36 +25,36 @@ const cooldownReset = computed({
 })
 
 const criticalModifier = computed(() => (critical.value ? 1.5 : 1))
-const cooldownModifier = computed(() => (cooldownReset.value ? 1 : 0.2))
 
 const damage = computed({
   get: () => {
-    // New formula:
+    if (fallHeight.value < 1.5) return (1 + 5) * criticalModifier.value
+
+    // Formula:
     // (baseDamage + falloff + 0.5 * densityLevel * fallHeight) * criticalModifier
     // fallOff = 4 * fallHeight                  when fallHeight <= 3
-    //         = 12 + 2 * (fallHeight - 3)       when fallHeight <= 8
+    //         = 12 + 2 * (fallHeight - 3)       when 3 < fallHeight <= 8
     //         = 12 + 10 + 1 * (fallHeight - 8)  when fallHeight > 8
     return (
-      6 +
-      (fallHeight.value <= 3
-        ? 4 * fallHeight.value
-        : fallHeight.value <= 8
-          ? 12 + 2 * (fallHeight.value - 3)
-          : 12 + 10 + 1 * (fallHeight.value - 8)) *
-        (0.5 * densityLevel.value * fallHeight.value) *
-        criticalModifier.value *
-        cooldownModifier.value
+      (1 + // player base damage
+        5 + // mace base damage
+        (fallHeight.value <= 3
+          ? 4 * fallHeight.value
+          : fallHeight.value <= 8
+            ? 12 + 2 * (fallHeight.value - 3)
+            : 22 + 1 * (fallHeight.value - 8)) + // falloff
+        0.5 * densityLevel.value * fallHeight.value) * // density bonus
+      criticalModifier.value
     )
   },
   set: (val) => {
-    const x = val / criticalModifier.value / cooldownModifier.value
+    const damage = val / criticalModifier.value
     fallHeight.value =
-      x < 18 + 1.5 * densityLevel.value
-        ? (x - 6) / (4 + 0.5 * densityLevel.value)
-        : x < 28 + 4 * densityLevel.value
-          ? (x - (18 + 1.5 * densityLevel.value)) / (2 + 0.5 * densityLevel.value) + 3
-          : (x - (28 + 4 * densityLevel.value)) / (1 + 0.5 * densityLevel.value) + 8
-    return fallHeight.value
+      damage > 28 + 4 * densityLevel.value
+        ? (damage - 20) / (1 + 0.5 * densityLevel.value)
+        : damage > 18 + 1.5 * densityLevel.value
+          ? (damage - 12) / (2 + 0.5 * densityLevel.value)
+          : (damage - 6) / (4 + 0.5 * densityLevel.value)
   },
 })
 
--- src/utils/iframe.ts
+++ src/utils/iframe.ts
@@ -2,5 +2,5 @@ export function parentOrigin(): string {
   const paramOrigin = new URLSearchParams(window.location.hash.substring(2)).get('origin')
   if (paramOrigin) return paramOrigin
 
-  return new URL(document.referrer).origin
+  return new URL(document.referrer ?? location.href).origin
 }
