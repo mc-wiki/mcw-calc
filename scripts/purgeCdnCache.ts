a27c0fe6858fa80589da66a1c2745c37e4307175
Dianliang233 <dianliang233@gmail.com>
2024-06-15 08:57:46 +0800

    Programmatically purge CF cache

---
+++ scripts/purgeCdnCache.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config'
+import { glob } from 'glob'
+
+const env = process.env
+
+if (env.WG_API_TOKEN === undefined) throw new Error('env.WG_API_TOKEN is not defined')
+
+const toolsHtml = await glob('src/tools/*/index.html')
+const toolsName = toolsHtml.map((path) => path.match(/src\/tools\/(.+)\/index.html/)![1])
+const toolsUrl = toolsName.map((name) => `https://tools.minecraft.wiki/static/tools/${name}/`)
+
+console.log('Purging:', toolsUrl)
+
+const response = await fetch('https://api.weirdgloop.org/purgecache', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Api-Key ${env.WG_API_TOKEN}`,
+  },
+  body: JSON.stringify({
+    site: 'minecraft.wiki', // CF zone
+    urls: toolsUrl,
+  }),
+})
+
+response.json().then(console.log)
