e9f10f900fe90e4c124ef4d2db8cc336cb1f030c
Dianliang233 <dianliang233@gmail.com>
2024-07-20 18:15:26 +0800

    Add notice if seedHashcode meet numerical or zero input

--- src/tools/seedHashcode/App.vue
+++ src/tools/seedHashcode/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { CdxTextInput } from '@wikimedia/codex'
+import { CdxMessage, CdxTextInput } from '@wikimedia/codex'
 import CalcField from '@/components/CalcField.vue'
 import { useI18n } from 'vue-i18n'
 import { hashCode } from '@/utils/seed'
@@ -13,8 +13,19 @@ const seed = ref('Minecraft Wiki')
 <template>
   <CalcField>
     <template #heading>{{ t('seedHashcode.title') }}</template>
-    <CdxTextInput v-model="seed" />
-    {{ t('seedHashcode.actual') }} <code v-if="seed !== ''">{{ hashCode(seed) }}</code
-    ><template v-else>{{ t('seedHashcode.emptyString') }}</template>
+    <div class="flex flex-col gap-2">
+      <CdxTextInput v-model="seed" />
+
+      <div>
+        {{ t('seedHashcode.actual') }}
+        <template v-if="seed === ''">{{ t('seedHashcode.emptyString') }}</template>
+        <template v-else-if="seed === '0'">{{ t('seedHashcode.zero') }}</template>
+        <code v-else>{{ hashCode(seed) }}</code>
+      </div>
+
+      <CdxMessage v-if="/^[0-9]*$/.test(seed)" type="warning">
+        {{ t('seedHashcode.number') }}
+      </CdxMessage>
+    </div>
   </CalcField>
 </template>
