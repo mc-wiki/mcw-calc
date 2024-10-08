22efa25a695fdef133c74c54f4bf5ad721634c7b
Dianliang233 <dianliang233@gmail.com>
2024-10-09 00:00:38 +0800

    Add th deploy target

--- scripts/deployConfig.ts
+++ scripts/deployConfig.ts
@@ -38,6 +38,12 @@ export const targets: DeployTarget[] = [
   },
   {
     type: 'production',
+    name: 'th',
+    apiUrl: 'https://th.minecraft.wiki/api.php',
+    credentials: 'PROD',
+  },
+  {
+    type: 'production',
     name: 'uk',
     apiUrl: 'https://uk.minecraft.wiki/api.php',
     credentials: 'PROD',
