d83cdff13410c962b6a7d99edda7dc840e3e8dab
Dianliang233 <dianliang233@gmail.com>
2024-12-09 15:27:23 +0800

    Upgrade dependencies

--- src/tools/enchantmentCost/main.ts
+++
@@ -1,7 +0,0 @@
-import plugin from '@/utils/plugin'
-import * as vue from 'vue'
-import App from './App.vue'
-
-const targetEl = document.querySelector('#app')!
-
-vue.createApp(App).use(plugin).mount(targetEl)
