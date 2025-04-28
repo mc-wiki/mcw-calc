a734cf30887bf93d11b1610eda812f82d3f74e65
Dianliang233 <dianliang233@gmail.com>
2025-04-28 10:00:31 +0800

    Use popover

--- src/tools/blockStructureRenderer/BsrPopup.vue
+++ src/tools/blockStructureRenderer/BsrPopup.vue
@@ -1,27 +1,18 @@
 <script setup lang="ts">
-import { flip, offset, shift, useFloating } from '@floating-ui/vue'
+import type { Icon } from '@wikimedia/codex-icons'
 import { onClickOutside } from '@vueuse/core'
-import { CdxButton, CdxIcon } from '@wikimedia/codex'
-import { cdxIconClose, type Icon } from '@wikimedia/codex-icons'
+import { CdxButton, CdxIcon, CdxPopover } from '@wikimedia/codex'
 import { ref } from 'vue'
-import { useI18n } from 'vue-i18n'
 
 const props = defineProps<{
   icon: Icon
   name: string
 }>()
 
-const { t } = useI18n()
-
 const button = ref()
 const popup = ref()
 const popupOpen = ref(false)
 
-const { floatingStyles: popupStyles } = useFloating(button, popup, {
-  open: popupOpen,
-  placement: 'top',
-  middleware: [offset(5), flip(), shift()],
-})
 onClickOutside(popup, () => {
   popupOpen.value = false
 })
@@ -36,33 +27,13 @@ onClickOutside(popup, () => {
   >
     <CdxIcon :icon="props.icon" :icon-label="props.name" />
   </CdxButton>
-  <div
-    v-if="popupOpen"
-    ref="popup"
-    style="
-      background-color: var(--background-color-base, #fff);
-      border: 1px solid var(--border-color-base, #a2a9b1);
-      border-radius: 4px;
-      padding: 12px;
-      width: fit-content;
-    "
-    :style="{ ...popupStyles, display: popupOpen ? 'block' : 'none' }"
+  <CdxPopover
+    v-model:open="popupOpen"
+    :title="props.name"
+    placement="top"
+    :anchor="button"
+    :use-close-button="true"
   >
-    <div
-      style="
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-bottom: 10px;
-      "
-    >
-      <h3 style="margin: 0">
-        {{ props.name }}
-      </h3>
-      <CdxButton weight="quiet" @click="popupOpen = false">
-        <CdxIcon :icon="cdxIconClose" :icon-label="t('blockStructureRenderer.closePopup')" />
-      </CdxButton>
-    </div>
     <slot />
-  </div>
+  </CdxPopover>
 </template>
