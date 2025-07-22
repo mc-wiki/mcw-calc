b12cd43daf8bc58b42866f553a49d05110d1c25a
teaSummer <see_tea@outlook.com>
2025-07-22 13:33:10 +0800

    Add dimensions param for blockDistribution
    Update global locales

--- src/tools/blockDistribution/main.ts
+++ src/tools/blockDistribution/main.ts
@@ -16,6 +16,7 @@ const i18n = createMcwI18n([import.meta.glob('./locale/*.json', { eager: true })
       blocks: sz.array(z.string()).default(['minecraft:diamond_ore']),
       blockNames: sz.array(z.string()).default(['Diamond Ore']),
       pageName: z.string().optional(),
+      dimensions: sz.array(z.string()).default(['overworld', 'nether', 'end']),
     })
     .safeParse(await getParams())
 
