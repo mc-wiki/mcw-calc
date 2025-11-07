a4a1b98d1604cd9306e2f641870c68b24470b1a8
Nickid Chen <nickid2018@outlook.com>
2025-11-07 17:16:56 +0800

    Fix lint error

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  ignores: ['dist', '**/dist/**', '**/locale/**', 'src/tools/lifeviewer/lv-viewer.js'],
+  ignores: ['dist', '**/dist/**', '**/locale/**', 'src/tools/lifeviewer/lv-plugin.js'],
   vue: true,
   stylistic: false,
   rules: {
