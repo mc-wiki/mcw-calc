69f554d7fac0f8ceae017ac7ff06402636d385dd
Nickid Chen <nickid2018@outlook.com>
2025-11-12 09:47:02 +0800

    Recover debug url

--- src/tools/protocol/constants.ts
+++ src/tools/protocol/constants.ts
@@ -1,6 +1,6 @@
 export const DATA_REPO =
-  // 'https://raw.githubusercontent.com/Nickid2018/MC_Protocol_Data/refs/heads/master'
-  'http://localhost:3001';
+  'https://raw.githubusercontent.com/Nickid2018/MC_Protocol_Data/refs/heads/master'
+// 'http://localhost:3001'
 export const VERSIONS_JSON = `${DATA_REPO}/java_edition/versions.json`
 export const INDEX_CSV = `${DATA_REPO}/java_edition/indexes.csv`
 export const PACKETS_CSV = `${DATA_REPO}/java_edition/packets.csv`
